957a604b1adad9e88d04c9f502f10f20c7de95a5
5juheon <ojuheon05@gmail.com>
2022-10-05 02:48:11 +0900

    Start project

---
+++ js/src.js
@@ -0,0 +1,25 @@
+	(window).scroll(function() {
+		$('#animatedElement').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideUp");
+			}
+		});
+	});
+
+	$('#animatedElement').click(function() {
+		$(this).addClass("slideUp");
+	});
+
+	const spyEls = document.querySelectorAll(".section.scroll-spy");
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl, //보여짐의 여부를 감시할 요소를 지정
+    triggerHook: 0.8,
+  })
+    //.setClassToggle(토글할요소, 넣었다뺄 Class이름)
+    .setClassToggle(spyEl, "show")
+    .addTo(new ScrollMagic.Controller());
+});
